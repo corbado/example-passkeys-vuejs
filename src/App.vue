a99ebd5ec755dc07dc6fba3314ea2b65626c6e1d
Lukas Kratzel <lukas.kratzel2004@gmail.com>
2024-04-25 14:40:23 +0200

    update example to use web-js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup>
-import {RouterView} from 'vue-router'
+import { RouterView } from "vue-router";
+import Corbado from "@corbado/web-js";
+import { ref } from "vue";
+
+const isInitialized = ref(false);
+
+Corbado.load({
+    projectId: import.meta.env.VITE_CORBADO_PROJECT_ID,
+    darkMode: "on",
+}).then(() => {
+    isInitialized.value = true;
+});
 </script>
 
 <template>
-  <RouterView/>
+    <div v-if="isInitialized">
+        <RouterView />
+    </div>
 </template>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,54 +1,48 @@
-<template>
-  <div>
-    <div v-if="user">
-      <h1>Profile Page</h1>
-      <p>
-        User-ID: {{ user.userID }}
-        <br/>
-        Email: {{ user.email }}
-      </p>
-      <button @click="handleLogout">Logout</button>
-    </div>
+<script setup>
+import { useRouter } from "vue-router";
+import Corbado from "@corbado/web-js";
+import { ref, onMounted } from "vue";
 
-    <!-- Show the "not logged in" message if the user is not logged in -->
-    <div v-else>
-      <p>You're not logged in.</p>
-      <p>Please go back to <a @click="redirectToHome">home</a> to log in.</p>
-    </div>
-  </div>
-</template>
+const user = ref(null);
+const router = useRouter();
 
-<script>
-import Corbado from '@corbado/webcomponent';
-import {ref, onMounted} from 'vue'
-import {useRouter} from 'vue-router'
-
-export default {
-  setup() {
-    const router = useRouter();
-    const user = ref(null);
-    const session = new Corbado.Session(import.meta.env.VITE_CORBADO_PROJECT_ID);
-
-    const handleLogout = () => {
-      session.logout()
-          .then(async () => {
-            await router.push('/');
-          })
-          .catch(err => console.log(err))
-    };
-
-    const redirectToHome = () => {
-      router.push('/');
-    }
-
-
-    onMounted(() => {
-      session.refresh(u => {
-        user.value = u;
-      })
-    });
-
-    return {user, handleLogout, redirectToHome}
-  }
+function redirectToHome() {
+    router.push("/");
 }
+
+async function handleLogout() {
+    user.value = null;
+    await Corbado.logout();
+    redirectToHome();
+}
+
+onMounted(() => {
+    user.value = Corbado.user;
+});
+
+onMounted(() => {
+    user.value = Corbado.user;
+});
 </script>
+
+<template>
+    <div>
+        <div v-if="user">
+            <h1>Profile Page</h1>
+            <p>
+                User-ID: {{ user.sub }}
+                <br />
+                Name: {{ user.name }}
+            </p>
+            <button @click="handleLogout">Logout</button>
+        </div>
+
+        <!-- Show the "not logged in" message if the user is not logged in -->
+        <div v-else>
+            <p>You're not logged in.</p>
+            <p>
+                Please go back to <a @click="redirectToHome">home</a> to log in.
+            </p>
+        </div>
+    </div>
+</template>
